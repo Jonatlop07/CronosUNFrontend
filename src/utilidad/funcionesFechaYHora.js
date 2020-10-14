b5d98f6b87898071ab46c8b5abe62ec8bad80520
Jonathan Lopez <jonatlop@gmail.com>
2020-10-14 12:25:14 -0500

    Se añaden dos funciones de utilidad: una para obtener la hora actual y otra para la fecha (ambas en el formato de los inputs de html).

---
+++ src/utilidad/funcionesFechaYHora.js
@@ -0,0 +1,15 @@
+export const obtenerFechaActual = () => {
+   const fechaActual = new Date();
+   const anio = fechaActual.getFullYear(),
+      mes = `0${fechaActual.getMonth()}`.slice(-2),
+      dia = `0${fechaActual.getDay()}`.slice(-2);
+
+   return `${anio}-${mes}-${dia}`;
+};
+
+export const obtenerHoraActual = () => {
+   const hora = `0${new Date().getHours()}`.slice(-2),
+      minuto = `0${new Date().getMinutes()}`.slice(-2);
+
+   return `${hora}:${minuto}`;
+};

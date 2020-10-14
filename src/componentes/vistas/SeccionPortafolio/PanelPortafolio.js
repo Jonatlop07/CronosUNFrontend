22d6f8b1f2d813240f815aa08f8a6e7804652c4f
Jonathan Lopez <jonatlop@gmail.com>
2020-10-14 08:25:28 -0500

    Se crean los archivos de estructura y estilo de los componentes que integran la sección del portafolio. Pendiente la definición de estos y la creación de subcomponentes para los mismos.

---
+++ src/componentes/vistas/SeccionPortafolio/PanelPortafolio.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PanelPortafolio = (props) => {
+   return <div></div>;
+};
+
+export default PanelPortafolio;
--- src/componentes/vistas/SeccionPortafolio/SeccionPortafolio.js
+++ src/componentes/vistas/SeccionPortafolio/SeccionPortafolio.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+
+import PanelPortafolio from "./PanelPortafolio.js";
+import SubSeccionDiario from "./SubseccionDiario.js";
+import SubSeccionPortafolio from "./SubseccionPortafolio.js";
 
 import "./estilos/seccionPortafolio.scss";
 
 const SeccionPortafolio = (props) => {
-    return ( 
-        <div>
-            
-        </div>
-     );
-}
- 
+   return (
+      <div className="seccion-portafolio">
+         <PanelPortafolio />
+         <div className="seccion-portafolio-subsecciones">
+            <SubSeccionDiario />
+            <SubSeccionPortafolio />
+         </div>
+      </div>
+   );
+};
+
 export default SeccionPortafolio;

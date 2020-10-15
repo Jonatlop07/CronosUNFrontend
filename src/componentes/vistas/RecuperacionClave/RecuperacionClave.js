5613a41f60756353c22d34c18580ce598888604c
Felipe <jrojasce@unal.edu.co>
2020-10-15 18:56:28 -0500

    Un gran inicio

---
+++ src/componentes/vistas/RecuperacionClave/RecuperacionClave.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./estilos/recuperacionClave.scss";
+
+const RecuperacionClave =(props) => {
+    return(
+        <div>
+            <p>Un gran inicio</p>
+        </div>
+    )
+}
+
+export default RecuperacionClave;

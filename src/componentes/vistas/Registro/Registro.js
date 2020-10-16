6729cf93045e54055bb139f86c03cad99653f5d0
jdcasallasm <jdcasallasm@unal.edu.co>
2020-10-15 21:17:14 -0500

    Adicion de estados

--- src/componentes/vistas/Registro/Registro.js
+++ src/componentes/vistas/Registro/Registro.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./estilos/registro.scss";
 
 const Registro = (props) => {
+  const [nombre, setNombre] = useState();
+  const [apellidos, setApellidos] = useState();
+  const [nombreDeUsuario, setNombreDeUsuario] = useState();
+  const [correo, setCorreo] = useState();
+  const [contrasena, setContrasena] = useState();
+
   return (
     <React.Fragment>
       <center>
         <h1>CronosUN</h1>
         <form>
-          <input type="text" placeholder="Nombre"/>
+          <input type="text" placeholder="Nombre" value = {nombre} autoFocus={true}/>
           <br />
-          <input type="text" placeholder="Apellidos"/>
+          <input type="text" placeholder="Apellidos" value = {apellidos}/>
           <br />
-          <input type="text" placeholder="Nombre de usuario"/>
+          <input type="text" placeholder="Nombre de usuario" value = {nombreDeUsuario}/>
           <br />
-          <input type="text" placeholder="Correo"/>
+          <input type="email" placeholder="Correo" value = {correo}/>
           <br />
-          <input type="password" placeholder="Contraseña"/>
+          <input type="password" placeholder="Contraseña" value = {contrasena}/>
           <br />
-          <input type="submit" value="Finalizar registro" />
+          <input type="submit" value="Finalizar registro"/>
           <br />
           <p>¿Ya posees una cuenta? <a>Inicia sesión</a></p>
         </form>

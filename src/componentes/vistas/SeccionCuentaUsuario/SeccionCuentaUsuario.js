f8da9105008f2abe7cb4f491c3cd890bba5200ed
VbarraganP <Vbarraganp@unal.edu.co>
2020-10-17 18:07:09 -0500

    modificacion del diseño de cuenta de usuario

--- src/componentes/vistas/SeccionCuentaUsuario/SeccionCuentaUsuario.js
+++ src/componentes/vistas/SeccionCuentaUsuario/SeccionCuentaUsuario.js
@@ -1,11 +1,77 @@
 import React from "react";
-
+import ImagenCuentaPorDefecto from "./Default.svg.png";
 import "./estilos/seccionCuentaUsuario.scss";
 
 const SeccionCuentaUsuario = (props) => {
   return (
     <div>
-      <p>Prueba</p>
+      <div className="contenedor">
+        <div className="vista-cuenta">
+          <img className="vista-cuenta-imagen" src={ImagenCuentaPorDefecto} />
+          <p className="vista-cuenta-nombreUsuario">Nombre de Usuario</p>
+          <button className="vista-cuenta-boton" type="submit">
+            Actualizar Imagen
+          </button>
+        </div>
+        <form className="editar-cuenta">
+          <p>
+            <label>Nombre</label>
+            <input
+              type="text"
+              name="Nombre"
+              className="editar-cuenta-entrada"
+            />
+          </p>
+          <p>
+            <label>Apellido</label>
+            <input
+              type="text"
+              name="Apellido"
+              className="editar-cuenta-entrada"
+            />
+          </p>
+          <p>
+            <label>Nombre de Usuario</label>
+            <input
+              type="text"
+              name="Nombre de Usuario"
+              className="editar-cuenta-entrada"
+            />
+          </p>
+          <p>
+            <label>Correo</label>
+            <input
+              type="text"
+              name="Correo"
+              className="editar-cuenta-entrada"
+            />
+          </p>
+          <p>
+            <label>Contraseña</label>
+            <input type="text" name="clave" className="editar-cuenta-entrada" />
+          </p>
+          <p>
+            <label>Biografia</label>
+            <input
+              type="text"
+              name="Biografia"
+              className="editar-cuenta-entrada"
+            />
+          </p>
+          <p>
+            <input
+              type="submit"
+              value="Guardar"
+              className="editar-cuenta-boton-enviar"
+            />
+          </p>
+        </form>
+      </div>
+      <div className="eliminar-perfil">
+        <button className="eliminar-perfil-boton" type="submit">
+          Eliminar Perfil
+        </button>
+      </div>
     </div>
   );
 };

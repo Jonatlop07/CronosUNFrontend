66b7abf312b03d17f68d49e9f9b378a2308e1b68
Felipe <jrojasce@unal.edu.co>
2020-10-16 19:20:55 -0500

    terminada

--- src/componentes/vistas/RecuperacionClave/RecuperacionClave.js
+++ src/componentes/vistas/RecuperacionClave/RecuperacionClave.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import "./estilos/recuperacionClave.scss";
 
-const RecuperacionClave =(props) => {
-    return(
-        <div>
-            <p>Un gran inicio</p>
+const RecuperacionClave = (props) => {
+    return (
+        <div className="recuperacion-clave">
+            <p className="recuperacion-clave-titulo">CronosUN</p>
+            <p className="recuperacion-clave-etiqueta">Ingrese su correo para validar el cambio de contraseña:</p>
+            <form>
+                <input type="text" name="Correo" className="recuperacion-clave-entrada"/>
+                <input type="submit" value="Enviar" className="recuperacion-clave-boton-enviar"/>
+            </form>
         </div>
-    )
+    ); 
 }
 
 export default RecuperacionClave;

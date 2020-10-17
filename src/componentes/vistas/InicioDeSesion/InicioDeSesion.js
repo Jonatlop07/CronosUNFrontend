35a6c91f5987b182fe335dcb204aaddebe534543
VbarraganP <Vbarraganp@unal.edu.co>
2020-10-17 13:32:08 -0500

    Inicio de sesion finalizada

--- src/componentes/vistas/InicioDeSesion/InicioDeSesion.js
+++ src/componentes/vistas/InicioDeSesion/InicioDeSesion.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import './estilos/inicioDeSesion.scss';
+import "./estilos/inicioDeSesion.scss";
 
 const InicioDeSesion = (props) => {
-    return (
-        <div>
-            <p>Prueba</p>
+  return (
+    <div className="inicio-sesion">
+      <p className="inicio-sesion-titulo"> CronosUN </p>
+      <form>
+        <input
+          type="text"
+          name="Correo"
+          className="inicio-sesion-entrada"
+          placeholder="Correo"
+        />
 
-        </div>
-    );
-}
- 
+        <input
+          type="text"
+          name="clave"
+          className="inicio-sesion-entrada"
+          placeholder="Contraseña"
+        />
+
+        <input
+          type="submit"
+          value="Iniciar sesion"
+          className="inicio-sesion-boton-enviar"
+        />
+      </form>
+      <p className="inicio-sesion-link">¿Olvisdaste tu contraseña? </p>
+      <p className="inicio-sesion-link">¿No tienes cuenta?</p>
+    </div>
+  );
+};
+
 export default InicioDeSesion;

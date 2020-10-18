d370f951975425f6a9f3451223695a21d3284cfa
Felipe <jrojasce@unal.edu.co>
2020-10-17 19:05:40 -0500

    primeros avance interfaz

--- src/componentes/vistas/SeccionHorarioAsignaturas/SeccionHorarioAsignaturas.js
+++ src/componentes/vistas/SeccionHorarioAsignaturas/SeccionHorarioAsignaturas.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 import "./estilos/seccionHorarioAsignaturas.scss";
-
+const Dias = ['Hora','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo']
+const Horas =['7-9','9-11','11-1','1-2','2-4','4-6','6-8']
 const SeccionHorarioAsignaturas = (props) => {
     return ( 
-        <div>
-            
+        <div className="contenedor-horario">
+            <div className='contenedor-horario-principal'>
+                {Dias.map((dia) => (
+                    <p>{dia}</p>
+                ))}
+            </div>
+            <div className='contenedor-horario-contenido'>
+                {Horas.map((hora) => (
+                    <p className='contenedor-horario-contenido-hora'>{hora}</p>
+                ))}
+            </div>
+            <input type="submit" value="Agregar asignatura" className="contenedor-horario-boton-agregar"/>
+            <div className ='contenedor-horario-materias'>
+                
+            </div>
         </div>
      );
 }

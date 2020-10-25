0fb57e14ac3a784ba37e8cb59f73015868eee133
Nicolas <wnarevalor@unal.edu.co>
2020-10-24 23:47:36 -0500

    responsive inicio

--- src/componentes/vistas/SeccionInicio/SeccionInicio.js
+++ src/componentes/vistas/SeccionInicio/SeccionInicio.js
@@ -2,59 +2,69 @@
 import React from 'react';
 
 import "./estilos/seccionInicio.scss";
+import { Link, animateScroll as scroll } from "react-scroll";
+import ImagenLogo from "../../../imagenes/img_logo.png";
+import dairy from "../../../imagenes/diary.png";
+import gato from "../../../imagenes/gato.png";
+import portfolio from "../../../imagenes/portfolio.png";
 
 const SeccionInicio = (props) => {
     return ( 
-        <div>
-           <header>
-                <div class = "header-Inicio">           
-                    <img class = "header-Inicio__imagen" src="https://storage.pixteller.com/designs/designs-images/2020-10-19/04/thereal-1-5f8cefce152e4.png" alt ="Logo CronosUN" />
-               </div>
-           </header>
-
+        <div className="seccion-inicio">       
+            <div className = "header-Inicio">           
+                <img className = "header-Inicio__imagen" src={ImagenLogo} alt ="Logo CronosUN" />
+                <Link to="sec2" smooth={true} duration={1000}><section className = "header-Inicio__scroller"></section></Link>
+            </div>
+           
             <main>
-                <section class="inicio">
-                        <p class = "inicio__p"> <span class = "inicio__CronosW">Cronos</span><span class="inicio__UN">UN</span> está orientado a facilitar la creación de tu horario
-                        de inscripción y a partir de este, crear horarios de estudio para que puedas organizar tu tiempo de manera
+                <section className="inicio" id="sec2">
+                        <section className ="inicio__contenedor1">
+                        <p className = "inicio__contenedor1__p"> Orientado a facilitar la creación de tu horario
+                        de inscripción y a partir de este, la creación de horarios de estudio para que puedas organizar tu tiempo de manera
                         fácil y rápida. </p>
-                        <img class = "inicio__img" src="https://storage.pixteller.com/designs/designs-images/2020-10-19/03/gatitobonito-1-5f8cd8d665c90.png" alt ="Imagen calendario"/>
                         </section>
-                <section class="inicio2">
-                        <p class = "inicio2__p"> Además, <span class = "inicio2__CronosW">Cronos</span><span class="inicio2__UN">UN</span> te permite crear y guardar proyectos,
+                        <section className ="inicio__contenedor2">
+                        <img className = "inicio__contenedor2__img" src={gato} alt ="Imagen calendario"/>
+                        </section>
+                </section>
+                <section className="inicio2">
+                        <section className ="inicio2__contenedor1"> <p className = "inicio2__contenedor1__par"> CronosUN te permite crear y guardar proyectos,
                         los cuales puedes subir a tu portafolio, y mostrarlo a los demas si así lo deseas. </p>
-                        <img class = "inicio2__img" src="https://storage.pixteller.com/designs/designs-images/2020-10-19/01/portfolio-1-5f8cc34061538.png" alt ="Imagen organizador"/>
                         </section>
-                <section class="inicio">       
-                        <p class= "inicio__p"> También puedes tener un diario personal, para registrar los avances hechos en un día
-                        o cualquier tipo de acontecimientos sucedidos y por supuesto, <span class = "inicio__CronosW">Cronos</span><span class="inicio__UN">UN</span> te ayudará 
-                        a realizar busquedas de lo escrito en tu diario cuando lo desees. </p>
-                        <img class= "inicio__img" src="https://storage.pixteller.com/designs/designs-images/2020-10-19/01/wip-1-5f8cc83051c18.png" alt = "Imagen diario"/>
-                
+                        <section className ="inicio2__contenedor2"><img className = "inicio2__contenedor2__imag" src={portfolio} alt ="Imagen organizador"/>
+                        </section>
+                </section>
+                <section className="inicio">   
+                        <section className ="inicio__contenedor1">      
+                        <p className = "inicio__contenedor1__p"> Te otorgamos un diario personal, para registrar los avances hechos cada día
+                        o cualquier tipo de acontecimientos sucedidos. ¡Y puedes hacer busquedas en este con un par de clicks!</p>
+                        </ section> 
+                        <section className ="inicio__contenedor2">
+                        <img className = "inicio__contenedor2__img" src={dairy} alt = "Imagen diario"/>
+                        </section>
                 </section>
 
-                <section class="empieza-ya">
-                    <a class="empieza-ya__prueba" href ="#!"> ¡Empieza a organizarte ahora!
-                    <span class="empieza-ya__span"><i class="fas fa-hourglass-start"></i> </span>
+                <section className="empieza-ya">
+                    <a className="empieza-ya__prueba" href ="#!"> ¡Empieza a organizarte ahora!
+                    <span className="empieza-ya__span"><i className="fas fa-hourglass-start"></i> </span>
                     </a>
                 </section>
 
             </main>
             
-            <footer class = "footer">
-                <h3 class = "footer__contactanos"> Contáctanos</h3>
-                <div class="footer__contacto"> 
-                    <div class= "footer__contacto__facebook">
-                        <i class="fab fa-facebook-square"></i> Facebook
-                        <div> <a class="footer__texto-secundario" href = "#!">CronosUN</a> </div>
+            <footer className = "footer">
+                <h3 className = "footer__contactanos"> Contáctanos</h3>
+                <div className="footer__contacto"> 
+                    <div className= "footer__contacto__facebook">
+                        <i className="fab fa-facebook-square"></i> Facebook
+                        <div> <a className="footer__texto-secundario" href = "#!">CronosUN</a> </div>
                     </div>
-                    <div class= "footer__contacto__correo">
-                        <i class="far fa-envelope"></i> Email
-                        <div class = "footer__texto-secundario">uncronos@gmail.com</div>
+                    <div className= "footer__contacto__correo">
+                        <i className="far fa-envelope"></i> Email
+                        <div className = "footer__texto-secundario">uncronos@gmail.com</div>
                     </div>
                 </div>
-            </footer>
-
-            
+            </footer>      
         </div>
      );
 }

c54f422dd351b5491b68dfecf9673577f46713f8
Felipe <jrojasce@unal.edu.co>
2020-10-26 15:32:17 -0500

    faltante: Analisis Texto y TF

--- src/componentes/vistas/SeccionInscripcionMaterias/ModalAgregarAsignatura.js
+++ src/componentes/vistas/SeccionInscripcionMaterias/ModalAgregarAsignatura.js
@@ -4,9 +4,23 @@ import ReactDOM from "react-dom";
 import "./estilos/modalAgregarAsignaturas.scss"; 
 
 const ModalAgregarAsignaturas = (props) => {
+    const [envioFormulario,setEnvioFormulario]= useState('');
+    const handleInputChange =(event) =>{
+        setEnvioFormulario({...envioFormulario,[event.target.name]: event.target.value})
+    }
+    const enviarFormulario =(event) =>{
+        event.preventDefault();
+        console.log(envioFormulario)
+    }
     return ReactDOM.createPortal(
-        <div>
-            <p>Listo</p>
+        <div className="contenedor-modal-agregar-asignatura">
+                <form className="contenedor-modal-agregar-asignatura-formulario" onSubmit={enviarFormulario}>
+                    <input type="text" className="contenedor-modal-agregar-asignatura-formulario-campotexto" onChange={handleInputChange}/>
+                    <div className ="contenedor-modal-agregar-asignatura-formulario-botones">
+                        <input type= "submit" className ="contenedor-modal-agregar-asignatura-formulario-botones-nueva-asignatura" value="Agregar nueva asignatura"/>
+                        <input type= "submit" className ="contenedor-modal-agregar-asignatura-formulario-botones-finalizar" value="Finalizar" onClick={props.terminarAgregandoAsignaturas} />
+                    </div> 
+                </form>
         </div>, document.getElementById("modal")
     );
 } 

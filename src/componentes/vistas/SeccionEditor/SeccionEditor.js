7130361d1d545ea3ac2ad35d898b9bee1ae8ac93
VbarraganP <Vbarraganp@unal.edu.co>
2020-10-27 20:14:22 -0500

    Inicio editor

--- src/componentes/vistas/SeccionEditor/SeccionEditor.js
+++ src/componentes/vistas/SeccionEditor/SeccionEditor.js
@@ -3,11 +3,14 @@ import React from "react";
 import "./estilos/seccionEditor.scss";
 
 const SeccionEditor = (props) => {
-   return (
-      <div>
-         <p></p>
-      </div>
-   );
+  return (
+    <div>
+      <p>
+        Holiwuie, soy un componente que no esta linkeado en la barra de
+        navegacion y en genera en ningun lado porque soy nuevo
+      </p>
+    </div>
+  );
 };
 
 export default SeccionEditor;

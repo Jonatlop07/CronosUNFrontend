a227a1b3d290ffa4baa2f53f845adaacdcd557fd
Jonathan Lopez <jonatlop@gmail.com>
2020-10-27 19:03:26 -0500

    Se añade la carpeta de la sección del editor en la carpeta vistas.

---
+++ src/componentes/vistas/SeccionEditor/SeccionEditor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./estilos/seccionEditor.scss";
+
+const SeccionEditor = (props) => {
+   return (
+      <div>
+         <p></p>
+      </div>
+   );
+};
+
+export default SeccionEditor;

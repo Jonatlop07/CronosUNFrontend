8b1700919bd229467e685cd4d3e8981e4d5b53a2
Felipe <jrojasce@unal.edu.co>
2020-10-30 10:40:45 -0500

    seccion terminada

--- src/componentes/vistas/RecuperacionClave/RecuperacionClave.js
+++ src/componentes/vistas/RecuperacionClave/RecuperacionClave.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import "./estilos/recuperacionClave.scss";
 
 const RecuperacionClave = (props) => {
+    const [envioCorreo,setEnvioCorreo]= useState('');
+    const handleInputChange =(event) =>{
+        setEnvioCorreo({...envioCorreo,[event.target.name]: event.target.value})
+    }
+    const enviarCorreo = (event) => {
+        event.preventDefault();
+        console.log(envioCorreo)
+    }
     return (
         <div className="recuperacion-clave">
             <p className="recuperacion-clave-titulo">CronosUN</p>
             <p className="recuperacion-clave-etiqueta">Ingrese su correo para validar el cambio de contraseña:</p>
-            <form>
-                <input type="text" name="Correo" className="recuperacion-clave-entrada"/>
+            <form onSubmit={enviarCorreo}>
+                <input type="text" name="Correo" className="recuperacion-clave-entrada"  onChange={handleInputChange}/>
                 <input type="submit" value="Enviar" className="recuperacion-clave-boton-enviar"/>
             </form>
         </div>

9914d97bb659a72ac50958cab65d74bbfdf41464
Jonathan Lopez <jonatlop@gmail.com>
2020-10-30 16:59:12 -0500

    Se termina la mayor parte del editor. Falta asegurarse que la opción de insertar código, insertar enlace e insertar imagen funcionen.

--- src/componentes/vistas/SeccionEditor/SeccionEditor.js
+++ src/componentes/vistas/SeccionEditor/SeccionEditor.js
@@ -5,29 +5,39 @@ import BarraHerramientas from "./BarraHerramientas.js";
 import "./estilos/seccionEditor.scss";
 
 const SeccionEditor = (props) => {
-  const pegarContenido = (e) => {
-    e.preventDefault();
-    const apertura = new RegExp("<", "gi");
-    const cierre = new RegExp(">", "gi");
-    const texto = (e.originalEvent || e).clipboardData
-      .getData("text/plain")
-      .replace(apertura, "&lt")
-      .replace(cierre, "&gt");
-    document.execCommand("insertHTML", false, texto);
-  };
+   const pegarContenido = (e) => {
+      e.preventDefault();
+      const apertura = new RegExp("<", "gi");
+      const cierre = new RegExp(">", "gi");
+      const texto = (e.originalEvent || e).clipboardData
+         .getData("text/plain")
+         .replace(apertura, "&lt")
+         .replace(cierre, "&gt");
+      document.execCommand("insertHTML", false, texto);
+   };
 
-  return (
-    <div className="editor">
-      <BarraHerramientas />
-      <div
-        className="editor-contenido"
-        id="editor"
-        contentEditable={true}
-        data-placeholder="Contenido..."
-        onPaste={(e) => pegarContenido(e)}
-      ></div>
-    </div>
-  );
+   const guardar = () => {
+      const contenido = document.getElementById("editor").innerHTML;
+      console.log(contenido);
+      // esta parte toca terminarla con la base de datos ( debe guardar "Publicacion" en la BD)
+   };
+
+   return (
+      <div className="editor">
+         <BarraHerramientas />
+         <div
+            className="editor-contenido"
+            id="editor"
+            contentEditable={true}
+            spellCheck={false}
+            data-placeholder="Contenido..."
+            onPaste={(e) => pegarContenido(e)}
+         ></div>
+         <button className="editor-guardar" onClick={(e) => guardar()}>
+            Guardar
+         </button>
+      </div>
+   );
 };
 
 export default SeccionEditor;

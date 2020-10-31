7ca350d17375b04199e83c9840ab1711f87b468c
Felipe <jrojasce@unal.edu.co>
2020-10-31 18:49:33 -0500

    Seccion terminada

--- src/componentes/vistas/SeccionInscripcionMaterias/ModalAgregarAsignatura.js
+++ src/componentes/vistas/SeccionInscripcionMaterias/ModalAgregarAsignatura.js
@@ -15,13 +15,16 @@ const ModalAgregarAsignaturas = (props) => {
     }
     return ReactDOM.createPortal(
         <div className="contenedor-modal-agregar-asignatura">
-                <form className="contenedor-modal-agregar-asignatura-formulario" onSubmit={enviarFormulario}>
-                    <textarea className="contenedor-modal-agregar-asignatura-formulario-campotexto" onChange={handleInputChange}/>
-                    <div className ="contenedor-modal-agregar-asignatura-formulario-botones">
-                        <input type= "submit" className ="contenedor-modal-agregar-asignatura-formulario-botones-nueva-asignatura" value="Agregar nueva asignatura"/>
-                        <input type= "submit" className ="contenedor-modal-agregar-asignatura-formulario-botones-finalizar" value="Finalizar" onClick={props.terminarAgregandoAsignaturas} />
+            <div className="modal-agregar-asignatura">
+                <h3 className="modal-agregar-asignatura-titulo">Inscripción de Asignaturas</h3>
+                <form className="modal-agregar-asignatura-formulario" onSubmit={enviarFormulario}>
+                    <textarea className="modal-agregar-asignatura-formulario-campotexto" onChange={handleInputChange} placeholder="Ingresa la información de la materia"/>
+                    <div className ="modal-agregar-asignatura-formulario-botones">
+                        <input type= "submit" className ="modal-agregar-asignatura-formulario-botones-nueva-asignatura" value="Agregar nueva asignatura"/>
+                        <input type= "submit" className ="modal-agregar-asignatura-formulario-botones-finalizar" value="Finalizar" onClick={props.terminarAgregandoAsignaturas} />
                     </div> 
                 </form>
+            </div>
         </div>, document.getElementById("modal")
     );
 } 

da3451227ffc1e303a28d4abbf5019bd1a055091
VbarraganP <Vbarraganp@unal.edu.co>
2020-12-05 19:08:53 -0500

    cambios en la autenticacion en App

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -19,18 +19,18 @@ import PieDePagina from "./PieDePagina.js";
 import "./estilos/app.scss";
 
 import {
-   RUTA_INICIO,
-   RUTA_INICIO_SESION,
-   RUTA_REGISTRO,
-   RUTA_REC_CLAVE,
-   RUTA_CUENTA_USUARIO,
-   RUTA_SEC_HORARIO_ASIG,
-   RUTA_SEC_HORARIO_PERSONAL,
-   RUTA_SEC_PORTAFOLIO,
-   RUTA_SEC_VISUALIZACION_PROYECTO,
-   RUTA_SEC_PROYECTOS_PUBLICOS,
-   RUTA_SEC_EDITOR,
-   RUTA_SEC_INSCRIPCION_MATERIAS,
+  RUTA_INICIO,
+  RUTA_INICIO_SESION,
+  RUTA_REGISTRO,
+  RUTA_REC_CLAVE,
+  RUTA_CUENTA_USUARIO,
+  RUTA_SEC_HORARIO_ASIG,
+  RUTA_SEC_HORARIO_PERSONAL,
+  RUTA_SEC_PORTAFOLIO,
+  RUTA_SEC_VISUALIZACION_PROYECTO,
+  RUTA_SEC_PROYECTOS_PUBLICOS,
+  RUTA_SEC_EDITOR,
+  RUTA_SEC_INSCRIPCION_MATERIAS,
 } from "../utilidad/rutas.js";
 
 function App() {
@@ -47,38 +47,38 @@ function App() {
           <Registro auth={setToken} idUsuario={setIdUsuario} />
         </Route>
         <Route exact path={RUTA_REC_CLAVE}>
-          <RecuperacionClave />
+          <RecuperacionClave auth={token} />
         </Route>
         <Route exact path={RUTA_INICIO}>
-          <SeccionInicio />
+          <SeccionInicio auth={token} idUsuario={idUsuario} />
         </Route>
         <Route exact path={RUTA_CUENTA_USUARIO}>
           <SeccionCuentaUsuario auth={token} idUsuario={idUsuario} />
         </Route>
         <Route exact path={RUTA_SEC_HORARIO_ASIG}>
-          <SeccionHorarioAsignaturas />
+          <SeccionHorarioAsignaturas auth={token} idUsuario={idUsuario} />
         </Route>
         <Route exact path={RUTA_SEC_HORARIO_PERSONAL}>
-          <SeccionHorarioPersonal />
+          <SeccionHorarioPersonal auth={token} idUsuario={idUsuario} />
         </Route>
         <Route exact path={RUTA_SEC_PORTAFOLIO}>
-          <SeccionPortafolio />
+          <SeccionPortafolio auth={token} idUsuario={idUsuario} />
         </Route>
         <Route exact path={RUTA_SEC_VISUALIZACION_PROYECTO}>
-          <VisualizacionProyecto />
+          <VisualizacionProyecto auth={token} idUsuario={idUsuario} />
         </Route>
         <Route exact path={RUTA_SEC_PROYECTOS_PUBLICOS}>
-          <SeccionConsultaPortafolio />
+          <SeccionConsultaPortafolio auth={token} idUsuario={idUsuario} />
         </Route>
         <Route exact path={RUTA_SEC_INSCRIPCION_MATERIAS}>
-          <SeccionInscripcionMaterias />
+          <SeccionInscripcionMaterias auth={token} idUsuario={idUsuario} />
         </Route>
         <Route exact path={RUTA_SEC_EDITOR}>
-          <SeccionEditor />
+          <SeccionEditor auth={token} idUsuario={idUsuario} />
         </Route>
       </Switch>
       <PieDePagina />
     </div>
   );
-
+}
 export default App;
--- src/componentes/vistas/SeccionHorarioAsignaturas/SeccionHorarioAsignaturas.js
+++ src/componentes/vistas/SeccionHorarioAsignaturas/SeccionHorarioAsignaturas.js
@@ -1,56 +1,70 @@
-import React,  { useState, useEffect }  from 'react';
-import ContenedorHorario from "./ContenedorHorario.js"
-import ContenedorAsignaturas from "./ContenedorAsignaturas.js"
-import {useHistory} from 'react-router-dom';
-import {RUTA_SEC_HORARIO_PERSONAL} from "../../../utilidad/rutas.js"
+import React, { useState, useEffect } from "react";
+import ContenedorHorario from "./ContenedorHorario.js";
+import ContenedorAsignaturas from "./ContenedorAsignaturas.js";
+import { useHistory } from "react-router-dom";
+import { RUTA_SEC_HORARIO_PERSONAL } from "../../../utilidad/rutas.js";
 import "./estilos/seccionHorarioAsignaturas.scss";
 
 const SeccionHorarioAsignaturas = (props) => {
-    const [horarioActualizado,setHorarioActualizado] = useState([])
-    const [materiasEliminadas,setMateriasEliminadas] = useState([])
-    const [asignaturasOpciones,setAsignaturasOpciones] = useState([])
-    const [horarioEnvio,setHorarioEnvio] = useState([])
-    const history = useHistory();
-    const routeChange = () =>{ 
-        let pathname = RUTA_SEC_HORARIO_PERSONAL; 
-        history.push({pathname,horarioEnvio});
-    }
-    const cargarAsignaturas = async () => {
-        const bearer = 'Bearer'+ auth;
-        const asignaturas = await fetch(
-            `http://localhost:8080/asignaturaOpciones?idUsuario=${1}`,{ 
-                withCredentials: true, 
-                credentials : 'include',
-                headers:{
-                    "Authorization": bearer,
-                    "Content-Type": "application.json",
-                },
-            }
-        ).then((respuesta) => respuesta.json());
-        setAsignaturasOpciones(asignaturas)
-    }   
-    const establecerHorario = async () => {
-
-    }
-    const agregarAsignatura = (materia,grupo) => {
-        const horarioAux = [...horarioActualizado];
-        horarioAux.push({materia,grupo})
-        setHorarioActualizado(horarioAux)
-    }
-    const desagregarAsignatura = (materia,grupo) => {
-        const materiasAux = [...materiasEliminadas];
-        materiasAux.push({materia,grupo})
-        setMateriasEliminadas(materiasAux)
-    }
-    useEffect(()=>{cargarAsignaturas()},[])
-    return ( 
-        <div className="contenedor-seccion">
-            <h5 className="contenedor-seccion-titulo">Mi horario de Inscripción</h5>
-            <ContenedorHorario horarioActualizado={horarioActualizado} setHorarioEnvio={setHorarioEnvio} materiasEliminadas={materiasEliminadas} />
-            <input type="submit" value="Guardar Horario" className="contenedor-seccion-boton-agregar" onClick={routeChange}/>
-            <ContenedorAsignaturas ListaMaterias={asignaturasOpciones} agregarAsignatura={agregarAsignatura} desagregarAsignatura={desagregarAsignatura} />
-        </div>
-     );
-}
+  const [horarioActualizado, setHorarioActualizado] = useState([]);
+  const [materiasEliminadas, setMateriasEliminadas] = useState([]);
+  const [asignaturasOpciones, setAsignaturasOpciones] = useState([]);
+  const [horarioEnvio, setHorarioEnvio] = useState([]);
+  const history = useHistory();
+  const routeChange = () => {
+    let pathname = RUTA_SEC_HORARIO_PERSONAL;
+    history.push({ pathname, horarioEnvio });
+  };
+  const cargarAsignaturas = async () => {
+    const bearer = "Bearer" + props.auth;
+    const asignaturas = await fetch(
+      `http://localhost:8080/asignaturaOpciones?idUsuario=${1}`,
+      {
+        withCredentials: true,
+        credentials: "include",
+        headers: {
+          Authorization: bearer,
+          "Content-Type": "application.json",
+        },
+      }
+    ).then((respuesta) => respuesta.json());
+    setAsignaturasOpciones(asignaturas);
+  };
+  const establecerHorario = async () => {};
+  const agregarAsignatura = (materia, grupo) => {
+    const horarioAux = [...horarioActualizado];
+    horarioAux.push({ materia, grupo });
+    setHorarioActualizado(horarioAux);
+  };
+  const desagregarAsignatura = (materia, grupo) => {
+    const materiasAux = [...materiasEliminadas];
+    materiasAux.push({ materia, grupo });
+    setMateriasEliminadas(materiasAux);
+  };
+  useEffect(() => {
+    cargarAsignaturas();
+  }, [cargarAsignaturas]);
+  return (
+    <div className="contenedor-seccion">
+      <h5 className="contenedor-seccion-titulo">Mi horario de Inscripción</h5>
+      <ContenedorHorario
+        horarioActualizado={horarioActualizado}
+        setHorarioEnvio={setHorarioEnvio}
+        materiasEliminadas={materiasEliminadas}
+      />
+      <input
+        type="submit"
+        value="Guardar Horario"
+        className="contenedor-seccion-boton-agregar"
+        onClick={routeChange}
+      />
+      <ContenedorAsignaturas
+        ListaMaterias={asignaturasOpciones}
+        agregarAsignatura={agregarAsignatura}
+        desagregarAsignatura={desagregarAsignatura}
+      />
+    </div>
+  );
+};
 
 export default SeccionHorarioAsignaturas;
